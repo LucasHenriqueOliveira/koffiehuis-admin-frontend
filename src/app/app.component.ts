1c8a0cf78af438ca5228bcbd79a2d1ca8a0abbd6
Lucas <lucashen@gmail.com>
2018-10-22 20:41:58 -0300

    verify if logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TokenService } from './services/token.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private Token: TokenService) { }
+
+  isLoggedIn() {
+    return this.Token.loggedIn();
+  }
 }
